b433a9dd0436c1eb92210e53010105c6737946c2
Programmer2753 <143874664+Programmer2753@users.noreply.github.com>
2026-01-24 00:05:08 +0200

    Add files via upload

--- db.js
+++ db.js
@@ -9,7 +9,7 @@ const pool = mysql.createPool({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   waitForConnections: true,
-  connectionLimit: 5, 
+  connectionLimit: 3, 
   queueLimit: 0,
   connectTimeout: 30000 
 });
