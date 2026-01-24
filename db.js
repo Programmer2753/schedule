ae6f77c1fa98b9366647d94dd7dabc3bf671bb5e
Programmer2753 <143874664+Programmer2753@users.noreply.github.com>
2026-01-24 13:05:49 +0200

    Add files via upload

--- db.js
+++ db.js
@@ -1,27 +1,15 @@
 import mysql from 'mysql2/promise';
 
-// Логируем попытку подключения (только для отладки)
-console.log("Попытка подключения к базе:", process.env.DB_HOST);
-
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   waitForConnections: true,
-  connectionLimit: 10, 
+  // Для бесплатной БД в Serverless среде ставим МИНИМУМ
+  connectionLimit: 1, 
   queueLimit: 0,
-  connectTimeout: 30000 
+  connectTimeout: 10000 
 });
-
-// Проверка связи при старте
-pool.getConnection()
-  .then(conn => {
-    console.log("Успешное подключение к БД!");
-    conn.release();
-  })
-  .catch(err => {
-    console.error("ОШИБКА ПОДКЛЮЧЕНИЯ К БД:", err.message);
-  });
 
 export default pool;
